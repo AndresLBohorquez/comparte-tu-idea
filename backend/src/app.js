d2b8539199db842aebf560c722e68842aca00cca
Andrés Leonardo Bohórquez <andresleonardo.6@gmail.com>
2022-10-30 21:35:05 -0500

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+		const app = express();
+		//configuraciones
+		app.set('port', process.env.PORT || 5000)
+
+		//Middlewares
+
+		//Routes
+
+		module.exports = app;
