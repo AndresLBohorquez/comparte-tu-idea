851c582f5dba041897629c6c7fd4e43b1fbb71f7
Andrés Leonardo Bohórquez <andresleonardo.6@gmail.com>
2022-11-01 22:59:40 -0500

    Actualización Front
    se realiza modificación, tomando datos de archivo .json, se crea screen para tema seleccionado, se adiciona footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import TopicScreen from './screens/TopicScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header>
-          <a href="/">Comparte tu idea</a>
+          <Navbar className='nav-color' variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>
+                  <img className='nav-logo' src='./images/logo.png' alt='img' />Comparte tu idea</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
-          
+          <Container>
+            <Routes>
+              <Route path="/topic/:slug" element={<TopicScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+
         </main>
+        <footer className='text-center'>
+          <div>@Derechos reservados Tatiana Rodríguez - Leonardo Bohórquez </div>
+          <small>2022</small>
+        </footer>
       </div>
     </BrowserRouter>
   );
