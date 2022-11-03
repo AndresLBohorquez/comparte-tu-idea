86b89c4aa26c02df9a1dcf793a37db9d4782efd1
Tatiana251 <tata.588@hotmail.com>
2022-11-02 20:27:34 -0500

    Revert "Merge branch 'master' of https://github.com/AndresLBohorquez/comparte-tu-idea"
    This reverts commit a02b839092d85ca23185204048a3a9aa63179ae5, reversing
    changes made to e72df67fd128ed2f70448db21d7f6bca909cd215.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,6 @@
 const express = require('express');
-const datafull = require('../data.json');
-const cors = require('cors');
 
 const app = express();
-
-app.use(cors());
-
-app.get('/api/topics', (req, res) =>{
-    res.send(datafull.topics);
-})
 
 //configuraciones
 app.set('port', process.env.PORT || 5000)
